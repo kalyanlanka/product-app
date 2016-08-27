ddb3d770fa1846f20201248c7b119e79c91ee32d
kalyanlanka <kalyan.lanka@convnimbus.com>
2016-08-27 19:56:21 -0400

    commit change in directory strictyre

--- server/config/main.js
+++
@@ -1,6 +0,0 @@
-const mongodburi = 'mongodb://productuser:products@ds147965.mlab.com:47965/productapp'
-module.exports = {
-    'database': mongodburi,
-    'secret': 'this is a secret passphrase',
-    'port': process.env.PORT || 3000
-}
